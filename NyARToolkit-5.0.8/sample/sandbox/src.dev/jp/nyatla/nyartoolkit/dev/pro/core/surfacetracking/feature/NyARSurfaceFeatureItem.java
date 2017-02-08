616aa98f664b60dbcc3072fdb81bc8b8013131b2
Eutrema <eutremasoysauce@gmail.com>
2017-02-08 13:56:19 +0900

    実行しやすくするためにファイル構成を変更
    リリースのために必要でないファイルを消去,およびファイル構成の変更
    
    natatmo_beacons_alldata_sample_attend.pdeにて180行目を同一階層のdataディレクトリを参照するように変更

--- NyARToolkit-5.0.8/sample/sandbox/src.dev/jp/nyatla/nyartoolkit/dev/pro/core/surfacetracking/feature/NyARSurfaceFeatureItem.java
+++
@@ -1,9 +0,0 @@
-package jp.nyatla.nyartoolkit.dev.pro.core.surfacetracking.feature;
-
-import jp.nyatla.nyartoolkit.core.types.NyARDoublePoint2d;
-
-public class NyARSurfaceFeatureItem extends NyARDoublePoint2d
-{
-    public NyARSurfaceFeatureSet.NyAR2FeatureCoord ref_feature;
-	public int scale;
-};
--- nyar4psg/examples/nftFilesGen/nftFilesGen.pde
+++
@@ -1,5 +0,0 @@
-import jp.nyatla.nyartoolkit.apps.nftfilegen.*;
-
-surface.setVisible(false);//for Processing 3.x later
-//frame.setVisible(false);//for Processing 2.x
-NyarNftGenApp.main(null);
